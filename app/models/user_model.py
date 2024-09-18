5bc5d3a252c97e53792e43f231286b044c45ca05
Rayankhalo7 <r.khalo@web.de>
2024-09-18 14:50:18 +0200

    mittwoch

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,11 +7,23 @@ from app import db  # Importiere die 'db' Instanz aus der App
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)  # E-Mail-Spalte hinzugefügt
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
+    
+    # Adresse in einzelne Felder unterteilt
+    street = db.Column(db.String(100), nullable=False)  # Straße
+    house_number = db.Column(db.String(10), nullable=False)  # Hausnummer
+    postal_code = db.Column(db.String(10), nullable=False)  # PLZ
+    city = db.Column(db.String(50), nullable=False)  # Ort
+    
+    # Telefonnummer
+    phone_number = db.Column(db.String(20), nullable=True)  # Telefonnummer, optional
+    
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())  # Erstellungsdatum
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
